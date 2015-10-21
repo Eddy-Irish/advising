1236925acd408d24722d270680029445c870c0f6
Keegan E. O'Hara <keohara@silk2.uvm.edu>
2015-10-21 09:14:27 -0400

    First commit of advising files

---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+
+<header>
+    <figure class="float_left small">
+            <img alt="A damn fine outfit" src="images/umphreys.jpg">
+            <figcaption>Do yourself a favor and check out Umphrey's</figcaption>
+    </figure>
+    <h1>Group Advising project site</h1>
+    <h2>Got Advising?</h2>
+</header>
+
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
